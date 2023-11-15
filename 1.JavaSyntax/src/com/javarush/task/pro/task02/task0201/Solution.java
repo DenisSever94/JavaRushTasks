75bae031e348776c211af00f78b504ea817b931f
Sedih Denis <beep5@yandex.ru>
2023-11-15 13:12:03 +0300

    add solution javaSyntax task 0115

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        int intValue = 10;
+        int numericValue = 20;
+        int size = 30;
+        int number = 40;
+
+    }
+}
