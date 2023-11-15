1d29ee0e8cfb16e5dd4de06cbd787724472202e6
Sedih Denis <beep5@yandex.ru>
2023-11-16 02:08:55 +0300

    add solution javaSyntax task 0203

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String word = "Hi";
+        String phrase = "Hello world";
+        String line = "Red";
+        String text = "Джава сучка";
+
+
+    }
+}
