17361d29fdaced7f1cc01a174d7c5b820e042a02
Sedih Denis <beep5@yandex.ru>
2023-11-16 21:59:10 +0300

    add solution javaSyntax task 0208

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0208/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0208/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
     public static void main(String[] args) {
         String emptiness = "пустота";
 
-        String fullness = ""; //напишите тут ваш код
+        String fullness = emptiness + " " + emptiness + " " + emptiness ; //напишите тут ваш код
 
         System.out.println(fullness);
     }
