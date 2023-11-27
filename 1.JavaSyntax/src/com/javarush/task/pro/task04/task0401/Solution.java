16f349b74bddac0b3e9fedee9398ad47c7965fb1
DenisSever <sedihdenis@bk.ru>
2023-11-27 19:57:38 +0300

    add solution javaSyntax task 0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        int number = 0;
+        while (number < 100 ) {
+            System.out.println(quote);
+            number++;
+
+        }
+
+    }
+}
