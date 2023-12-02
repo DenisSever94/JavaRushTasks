4696a45813e5bb74957ee95fe0d0830489c1e621
DenisSever <sedihdenis@bk.ru>
2023-12-02 07:36:31 +0300

    add solution javaSyntax task 0413

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0413;
+
+/* 
+Рисуем треугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 9; i++) {
+
+           int start = 0 + i;
+            for (int j = 0; j <= start; j++) {
+
+                System.out.print("8");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
