e82d0aa9eaa9727bccfa13600de03d534d96c217
DenisSever <sedihdenis@bk.ru>
2023-12-05 01:24:27 +0300

    add solution javaSyntax task 0504

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0504/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task05.task0504;
+
+/* 
+Объединяем массивы
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    public static int[] resultArray;
+
+    public static void main(String[] args) {
+
+        resultArray = new int[firstArray.length + secondArray.length];
+
+        for (int q = 0; q < firstArray.length; q++) {
+            resultArray[q] = firstArray[q];
+
+        }
+        for (int e = 0; e < secondArray.length; e++) {
+            resultArray[firstArray.length + e] = secondArray[e];
+        }
+
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.print(resultArray[i] + ", ");
+
+        }
+
+
+    }
+}
