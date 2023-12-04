c97e4f11a871eb358de00ce6abec2ef8a1cb2778
DenisSever <sedihdenis@bk.ru>
2023-12-04 13:53:48 +0300

    add solution javaSyntax task 0416

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task04.task0416;
+
+import java.util.Scanner;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        double result =(1.0 * number1) / number2;
+        System.out.println(result);
+
+    }
+}
