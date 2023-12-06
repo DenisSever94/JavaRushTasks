447bbf9b18db21f585dc8b812729586aad403078
DenisSever <sedihdenis@bk.ru>
2023-12-06 07:59:13 +0300

    add solution javaSyntax task 0505

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task05.task0505;
+
+import java.util.Scanner;
+
+/* 
+Reverse
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
