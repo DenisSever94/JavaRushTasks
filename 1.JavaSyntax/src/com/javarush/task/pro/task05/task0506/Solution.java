ee02ed6b1f82f061a4e072d61dbd896e9acdb046
DenisSever <sedihdenis@bk.ru>
2023-12-06 07:59:31 +0300

    add solution javaSyntax task 0506

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0506/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task05.task0506;
+
+import java.util.Scanner;
+
+/* 
+Минимальное из N чисел
+*/
+
+public class Solution {
+    public static int[] array = new int[5];
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            array[i] = scanner.nextInt();
+
+        }
+        int min = array[0];
+        for (int i = 0; i < 5; i++) {
+            if (array[i] < min)
+                min = array[i];
+        }
+        System.out.println(min);
+
+    }
+
+
+}
+
