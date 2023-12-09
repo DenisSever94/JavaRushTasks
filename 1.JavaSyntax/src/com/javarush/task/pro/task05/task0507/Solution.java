6155dc5264754a3f778a7527bcaf2b1bf0fcbf76
DenisSever <sedihdenis@bk.ru>
2023-12-10 00:16:48 +0300

    add solution javaSyntax task 0507

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task05.task0507;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static int[] array;
+
+    public static void main(String[] args) throws Exception {
+     Scanner scanner = new Scanner(System.in);
+     int size = scanner.nextInt();
+     array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        } int max = array[0];
+        for (int i = 0; i < size; i++) {
+            if (array[i] > max)
+                max = array[i];
+
+        }
+        System.out.println(max);
+    }
+
+}
