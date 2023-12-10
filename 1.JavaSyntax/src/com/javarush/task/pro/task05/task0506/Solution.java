9099c6035d11a86bb80f93e70e6ee9a18c8ceb4a
DenisSever <sedihdenis@bk.ru>
2023-12-10 08:38:46 +0300

    add solution javaSyntax task 0508

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0506/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0506/Solution.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static int[] array;
+    public static int[] myArray;
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         int size = scanner.nextInt();
-        array = new int[size];
+        myArray = new int[size];
         for (int i = 0; i < size; i++) {
-            array[i] = scanner.nextInt();
+            myArray[i] = scanner.nextInt();
 
         }
-        int min = array[0];
+        int min = myArray[0];
         for (int i = 0; i < size; i++) {
-            if (array[i] < min)
-                min = array[i];
+            if (myArray[i] < min)
+                min = myArray[i];
         }
         System.out.println(min);
 
