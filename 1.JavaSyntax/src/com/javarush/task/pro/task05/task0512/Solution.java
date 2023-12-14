ad9b66cc0fd411563324a94d75fe900f705e3288
DenisSever <sedihdenis@bk.ru>
2023-12-14 07:42:17 +0300

    add solution javaSyntax task 0512

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0512/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task05.task0512;
+
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}}, {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
+
+    public static void main(String[] args) {
+
+         String s = Arrays.deepToString(multiArray);
+        System.out.println(s);
+    }
+}
