c1fce990c7872c682e71395706df36964eef89c2
DenisSever <sedihdenis@bk.ru>
2023-12-15 10:21:58 +0300

    add solution javaSyntax task 0519

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task05.task0519;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static int element = 5;
+
+    public static void main(String[] args) {
+        int[] array1 = array.clone();
+        Arrays.sort(array1);
+        int number = Arrays.binarySearch(array1, element);
+        boolean result = number >= 0;
+        System.out.println(result);
+    }
+}
