33f3725393406df9fa52158168a53e28533df485
DenisSever <sedihdenis@bk.ru>
2023-12-22 00:17:51 +0300

    add solution javaSyntax task 0505

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -1,13 +1,46 @@
 package com.javarush.task.pro.task05.task0505;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-/* 
-Reverse
-*/
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int[] num;
+
+        if (number > 0) {
+            num = new int[number];
+            if (number % 2 != 0) {
+                for (int i = 0; i < num.length; i++) {
+                    num[i] = scanner.nextInt();
+                    System.out.println(Arrays.toString(num));
+
+                }
+            }
+//            number = scanner.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+//        int[] num;
+//        num = new int[scanner.nextInt()];
+//        if (num.length > 0) {
+//            if (num.length % 2 !=0 ) {
+//
+//
+//            }
+
+
+        }
     }
 }
