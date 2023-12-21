5dff550b76baf016c68ab072467de263d0f34cbb
DenisSever <sedihdenis@bk.ru>
2023-12-21 17:29:44 +0300

    add solution javaSyntax task 0510

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -1,14 +1,29 @@
 package com.javarush.task.pro.task05.task0510;
 
-/* 
-Треугольный массив
-*/
+import java.util.Arrays;
 
 public class Solution {
-
     public static int[][] result = new int[10][];
-
     public static void main(String[] args) {
-        //напишите тут ваш код
+        result[0] = new int[1];
+        result[1] = new int[2];
+        result[2] = new int[3];
+        result[3] = new int[4];
+        result[4] = new int[5];
+        result[5] = new int[6];
+        result[6] = new int[7];
+        result[7] = new int[8];
+        result[8] = new int[9];
+        result[9] = new int[10];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new int[i + 1];
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = i + j;
+                System.out.print(" " + result[i][j]);
+            }
+            System.out.println();
+        }
+
     }
+
 }
