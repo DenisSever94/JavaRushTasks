3132e2914ce309df2266d4f965e2cf3b70079fdb
DenisSever <sedihdenis@bk.ru>
2023-12-22 00:17:26 +0300

    add solution javaSyntax task 0502

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0502/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0502/Solution.java
@@ -1,7 +1,7 @@
 package com.javarush.task.pro.task05.task0502;
 
 public class Solution {
-    public static final String ODD1 = "Нечётный";
+    public static final String ODD2 = "Нечётный";
     public static final String EVEN = "Чётный";
     public static String[] strings = new String[5];
 
