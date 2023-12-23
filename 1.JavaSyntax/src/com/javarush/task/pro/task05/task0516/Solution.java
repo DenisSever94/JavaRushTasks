5117b0c4912cc9a175ca6594099533de725c819a
DenisSever <sedihdenis@bk.ru>
2023-12-23 21:12:47 +0300

    add solution javaSyntax task 0506

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
@@ -4,27 +4,18 @@ import java.util.Arrays;
 
 public class Solution {
 
-    public static int[] array = new int[22];
+    public static int[] array = new int[21];
     public static int valueStart = 10;
     public static int valueEnd = 13;
 
     public static void main(String[] args) {
+        /*
 
+         */
 
-        int mid = array.length / 2;
-        for (int i = 0; i < array.length; i++) {
-            if (array.length % 2 == 0) { // проверяем делится ли массив
+        Arrays.fill(array, 0, 11, valueStart);
+        Arrays.fill(array, 11, 21, valueEnd);
 
-//                  array[i] = valueStart;// заполнить массив до половины start
-//                } else {
-//                    array[i] = valueEnd; //
-//                    if (i <= mid) {
-//                        array[i] = valueStart;
-//                    } else {
-//                        array[i] = valueEnd;
-
-                    } System.out.println(Arrays.toString(array));
-                }
-            }
-        }
-
+        System.out.println(Arrays.toString(array));
+    }
+}
