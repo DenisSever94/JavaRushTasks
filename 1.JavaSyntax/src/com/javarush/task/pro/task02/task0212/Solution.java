b4824cd3d2f540933348b0ead9b54f11b8293fbc
DenisSever <sedihdenis@bk.ru>
2023-12-26 19:02:19 +0300

    add solution javaSyntax task 0502

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0212/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0212/Solution.java
@@ -7,12 +7,12 @@ package com.javarush.task.pro.task02.task0212;
 public class Solution {
 
     public static void main(String[] args) {
-        String caps = "if I type in caps ";
+        String caps1 = "if I type in caps ";
         String usa = "сша";
 
         //напишите тут ваш код
         System.out.println(usa.toUpperCase());
         System.out.println("Винни Пух".toUpperCase());
-        System.out.println(caps.toUpperCase()+ "they know I mean business".toUpperCase());
+        System.out.println(caps1.toUpperCase()+ "they know I mean business".toUpperCase());
     }
 }
