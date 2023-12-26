50fe0ec4157fe62d995f069d5b271d205e109f66
DenisSever <sedihdenis@bk.ru>
2023-12-26 19:02:49 +0300

    add solution javaSyntax task 0502

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 
 
 public class Solution {
-    public static String[] strings;
+    public static String[] strings1;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        strings = new String[6];
-        for (int i = 0; i < strings.length; i++) {
-            strings[i] = scanner.nextLine();
+        strings1 = new String[6];
+        for (int i = 0; i < strings1.length; i++) {
+            strings1[i] = scanner.nextLine();
        }
-        for (int i = 0; i < strings.length - 1; i++) {
-            String srt = strings[i];
-            for (int j = i + 1; j < strings.length; j++) {
+        for (int i = 0; i < strings1.length - 1; i++) {
+            String srt = strings1[i];
+            for (int j = i + 1; j < strings1.length; j++) {
 
                 if (srt == null) {
                     break;
                 }
-                if (srt.equals(strings[j])) {
-                    strings[i] = null;
-                    strings[j] = null;
+                if (srt.equals(strings1[j])) {
+                    strings1[i] = null;
+                    strings1[j] = null;
 
                 }
             }
         }
 
-        for (int i = 0; i < strings.length; i++) {
-            System.out.print(strings[i] + ", ");
+        for (int i = 0; i < strings1.length; i++) {
+            System.out.print(strings1[i] + ", ");
         }
     }
 }
