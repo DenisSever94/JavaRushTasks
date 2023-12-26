c64003216f54208e1cbf4ee6712a23703d72ffac
DenisSever <sedihdenis@bk.ru>
2023-12-26 19:02:10 +0300

    add solution javaSyntax task 0502

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
@@ -1,15 +1,12 @@
 package com.javarush.task.pro.task02.task0203;
 
-/* 
-Последняя цифра числа
-*/
 
 public class Solution {
 
     public static void main(String[] args) {
         int number = 546;
 
-        int lastDigit = number % 10; //напишите тут ваш код
+        int lastDigit = number % 10;
 
         System.out.println(lastDigit);
     }
