ba5974ed4755f08fb0af3851339d647626dff7a4
DenisSever <sedihdenis@bk.ru>
2023-12-27 23:56:12 +0300

    add solution javaSyntax task 0502

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
@@ -3,17 +3,17 @@ package com.javarush.task.pro.task05.task0509;
 
 public class Solution {
 
-    public static int[][] MULTIPLICATION_TABLE = new int[10][10];
+    public static int[][] MULTIPLICATION_TABLE1 = new int[10][10];
 
     public static void main(String[] args) {
-        for (int i = 0; i < MULTIPLICATION_TABLE.length; i++) {
-            for (int j = 0; j < MULTIPLICATION_TABLE[i].length; j++) {
-                MULTIPLICATION_TABLE[i][j] = (i + 1) * (j + 1);
+        for (int i = 0; i < MULTIPLICATION_TABLE1.length; i++) {
+            for (int j = 0; j < MULTIPLICATION_TABLE1[i].length; j++) {
+                MULTIPLICATION_TABLE1[i][j] = (i + 1) * (j + 1);
             }
         }
-        for (int i = 0; i < MULTIPLICATION_TABLE.length; i++) {
-            for (int j = 0; j < MULTIPLICATION_TABLE[i].length; j++) {
-                System.out.print(MULTIPLICATION_TABLE[i][j] + " ");
+        for (int i = 0; i < MULTIPLICATION_TABLE1.length; i++) {
+            for (int j = 0; j < MULTIPLICATION_TABLE1[i].length; j++) {
+                System.out.print(MULTIPLICATION_TABLE1[i][j] + " ");
 
             } System.out.println();
         }
