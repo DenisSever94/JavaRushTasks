46d8ff819df0253fbc4cd672484ab15617e96e1a
DenisSever <sedihdenis@bk.ru>
2023-12-27 23:55:48 +0300

    add solution javaSyntax task 0502

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -8,39 +8,23 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        int[] num;
-
-        if (number > 0) {
-            num = new int[number];
-            if (number % 2 != 0) {
-                for (int i = 0; i < num.length; i++) {
-                    num[i] = scanner.nextInt();
-                    System.out.println(Arrays.toString(num));
-
-                }
-            }
-//            number = scanner.nextInt();
-
-
-
-
-
-
-
-
-
-
-
-
-//        int[] num;
-//        num = new int[scanner.nextInt()];
-//        if (num.length > 0) {
-//            if (num.length % 2 !=0 ) {
-//
-//
-//            }
-
+        int[] array = new int[number];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
 
         }
+
+        if (number % 2 != 0) {
+            for (int i = 0; i < number; i++) {
+                System.out.println(array[i]);
+            }
+        } else {
+            for (int i = array.length - 1; i >= 0; i--) {
+                System.out.println(array[i]);
+
+            }
+        }
+
+
     }
 }
