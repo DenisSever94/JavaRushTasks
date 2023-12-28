f41b71a88a34e8256b92f3fd4e49be83ed406ade
DenisSever <sedihdenis@bk.ru>
2023-12-28 16:03:07 +0300

    add solution javaSyntax task 0608

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task06.task0608;
+
+/* 
+Кубический калькулятор
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        cube(3);
+    }
+
+
+    public static long cube(long number) {
+        return number * number * number;
+
+
+    }
+}
