f6147219cba1c5bd39949883538b6b9a4ecc6d2a
DenisSever <sedihdenis@bk.ru>
2023-12-28 16:02:00 +0300

    add solution javaSyntax task 0404

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -1,21 +1,18 @@
 package com.javarush.task.pro.task04.task0404;
 
-/* 
-Заполненный прямоугольник
-*/
 
 public class Solution {
     public static void main(String[] args) {
-        int line = 0;
+        int line2 = 0;
 
-        while (line < 5) {
+        while (line2 < 5) {
             int line1 = 0;
             while (line1 < 9) {
                 System.out.print("Q");
                 line1++;
             }
             System.out.println("Q");
-            line++;
+            line2++;
         }
     }
 }
