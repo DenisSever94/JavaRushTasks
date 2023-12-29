7fc7f4653cb9ca26269cbdbd892a478cce1cdf46
DenisSever <sedihdenis@bk.ru>
2023-12-29 12:27:01 +0300

    add solution javaSyntax task 0505

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0506/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0506/Solution.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static int[] array1;
+    public static int[] array2;
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         int size = scanner.nextInt();
-        array1 = new int[size];
+        array2 = new int[size];
         for (int i = 0; i < size; i++) {
-            array1[i] = scanner.nextInt();
+            array2[i] = scanner.nextInt();
 
         }
-        int min = array1[0];
+        int min = array2[0];
         for (int i = 0; i < size; i++) {
-            if (array1[i] < min)
-                min = array1[i];
+            if (array2[i] < min)
+                min = array2[i];
         }
         System.out.println(min);
 
