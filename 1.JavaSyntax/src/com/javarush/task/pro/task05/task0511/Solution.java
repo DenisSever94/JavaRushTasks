ffc0f092a2c1035aa24ae2a1bedd47743c130043
DenisSever <sedihdenis@bk.ru>
2023-12-30 23:16:41 +0300

    add solution javaSyntax task 0505

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -5,19 +5,19 @@ import java.util.Arrays;
 
 
 public class Solution {
-    public static int[][] multiArray;
+    public static int[][] multiArray1;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
 
-        multiArray = new int[num][];
+        multiArray1 = new int[num][];
         for (int i = 0; i < num; i++) {
             int num2 = scanner.nextInt();
-            multiArray[i] = new int[num2];
+            multiArray1[i] = new int[num2];
 
         }
-        System.out.println(Arrays.deepToString(multiArray));
+        System.out.println(Arrays.deepToString(multiArray1));
 
 
     }
