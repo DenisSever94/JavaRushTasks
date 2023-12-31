b9ff25c72d40826a12fed26190383957974a80fa
DenisSever <sedihdenis@bk.ru>
2023-12-31 23:26:01 +0300

    add solution javaSyntax task 0505

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0515/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0515/Solution.java
@@ -1,14 +1,19 @@
 package com.javarush.task.pro.task05.task0515;
 
 import java.util.Arrays;
+
+/* 
+Сравнение двумерных массивов
+*/
+
 public class Solution {
 
-    public static int[][] arrayFirst = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-    public static int[][] arraySecond = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    public static int[][] arrayFirst1 = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    public static int[][] arraySecond2 = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 
     public static void main(String[] args) {
 
-       boolean s = Arrays.deepEquals(arrayFirst, arraySecond);
+       boolean s = Arrays.deepEquals(arrayFirst1, arraySecond2);
 
         System.out.println(s);
     }
