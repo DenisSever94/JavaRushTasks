af37a55340df8468d2598dec3796872fcdd0199e
DenisSever <sedihdenis@bk.ru>
2023-12-31 23:25:55 +0300

    add solution javaSyntax task 0505

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0514/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0514/Solution.java
@@ -7,11 +7,11 @@ import java.util.Arrays;
 public class Solution {
 
     public static String[][] strings = new String[][]{{"🙂", "🙂", "🙂", "🙂"}, {"🙃", "🙃", "🙃", "🙃"}, {"😋", "😋", "😋", "😋"}, {"😎", "😎", "😎", "😎"}};
-    public static int[][] ints = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    public static int[][] ints1 = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
 
     public static void main(String[] args) {
         String str = Arrays.deepToString(strings);
-        String str1 = Arrays.deepToString(ints);
+        String str1 = Arrays.deepToString(ints1);
         System.out.println(str);
         System.out.println(str1);
     }
