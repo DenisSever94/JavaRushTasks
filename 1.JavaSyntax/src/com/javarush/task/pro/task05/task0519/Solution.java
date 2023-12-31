fb4432e3f7a9bd3cb52a51743a57a572897b81b7
DenisSever <sedihdenis@bk.ru>
2023-12-31 23:26:11 +0300

    add solution javaSyntax task 0505

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class Solution {
 
     public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    public static int element1 = 22;
+    public static int element = 22;
 
     public static void main(String[] args) {
         int[] array1 = array.clone();
         Arrays.sort(array1);
-        int number = Arrays.binarySearch(array1, element1);
+        int number = Arrays.binarySearch(array1, element);
         boolean result = number >= 0;
         System.out.println(result);
     }
