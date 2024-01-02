cd625fabfb51f4b84d12a3a71234385434eb723e
DenisSever <sedihdenis@bk.ru>
2024-01-02 20:02:42 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0615/Earth.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task06.task0615;
+
+/* 
+Все что нужно знать о Земле
+*/
+
+public class Earth {
+    String name = "Земля";
+    double square = 510_100_000;
+    long population = 7_594_000_000L;
+    long equatorLength = 40_075_696;
+}
