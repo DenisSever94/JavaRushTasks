8dc8452cdd988e1c81464f3bad15d3f65931fc7d
DenisSever <sedihdenis@bk.ru>
2024-01-02 19:03:40 +0300

    add solution javaSyntax task 0505

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -1,8 +1,6 @@
 package com.javarush.task.pro.task06.task0605;
 
-/* 
-Правильный порядок
-*/
+
 
 public class Solution {
 
@@ -14,7 +12,13 @@ public class Solution {
     }
 
     public static void reverseArray(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
 
+
+        }
     }
 
     public static void printArray(int[] array) {
