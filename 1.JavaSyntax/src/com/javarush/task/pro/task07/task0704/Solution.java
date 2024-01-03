df20f40743796ceae6f3f554e1d2f6e28e35ba79
DenisSever <sedihdenis@bk.ru>
2024-01-03 11:55:18 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0704/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0704;
+
+/* 
+Экспонентная запись числа
+*/
+
+public class Solution {
+    double earthDiameter = 1.2742e+4;
+    double lightSpeed = 2.99792458e+8;
+    double uraniumAtomicMass = 2.380289e+2;
+    double averageBeeWeight = 8.5e-2;
+    double javaDeveloperSalary = 1e+4;
+}
