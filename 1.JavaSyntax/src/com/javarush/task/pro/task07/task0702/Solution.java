74999640411f65430f1e4713f51be10d2a993662
DenisSever <sedihdenis@bk.ru>
2024-01-03 11:55:01 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+   byte b = 127;
+   short s = 32767;
+   int i = 2147483647;
+   long l = 9223372036854775807L;
+}
