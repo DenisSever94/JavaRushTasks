a7978a225bdc0ea294dc8490bbd060810579309f
DenisSever <sedihdenis@bk.ru>
2024-01-04 07:54:49 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0706/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task07.task0706;
+
+/* 
+Странное деление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        div(0d, 0d);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
