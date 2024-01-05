72844d98ac244c4c3672d70f1da595726b42978e
DenisSever <sedihdenis@bk.ru>
2024-01-06 02:12:14 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0801/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task08.task0801;
+
+import static java.lang.Math.toRadians;
+
+public class Solution {
+
+    public static double sin(double a) {
+
+        return sin(Math.sin(Math.toRadians(45)));
+    }
+
+    public static double cos(double a) {
+
+
+        return Math.cos(Math.toRadians(45));
+    }
+
+    public static double tan(double a) {
+
+     double d =   Math.tan(Math.toDegrees(45));
+        return d;
+    }
+}
