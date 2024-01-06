e9034e228d6737fde32ab560103d7c2103c818c0
DenisSever <sedihdenis@bk.ru>
2024-01-07 01:29:22 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0803/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task08.task0803;
+
+import java.util.Scanner;
+
+/* 
+Минимальный элемент массива
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+
+        int[] intArray = getArrayOfTenElements();
+        System.out.println(min(intArray));
+    }
+
+    public static int min(int[] ints) {
+        //напишите тут ваш код
+        return 0;
+    }
+
+    public static int[] getArrayOfTenElements() {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        for (int i = 0; i <9 ; i++) {
+
+            return new int[10];
+        }
+    }
+}
