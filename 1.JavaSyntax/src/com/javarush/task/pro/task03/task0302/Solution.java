9a756b4605387bb543f000cc575c096679d388de
DenisSever <sedihdenis@bk.ru>
2024-01-08 21:53:10 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        String militaryCommissar1 = ", явитесь в военкомат";
+        String militaryCommissar = ", явитесь в военкомат";
 //        Scanner name = new Scanner(System.in);
         Scanner console = new Scanner(System.in);
         String name = console.nextLine();
@@ -16,7 +16,7 @@ public class Solution {
         int yourAge = console.nextInt();
 
         if (yourAge >= 18 && yourAge <= 28)  {
-            System.out.println(name + militaryCommissar1);
+            System.out.println(name + militaryCommissar);
 
         }
 
