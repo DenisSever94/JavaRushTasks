bcd46fa20cc3ddf5329a9fa7aaa540901500c4f3
DenisSever <sedihdenis@bk.ru>
2024-01-09 19:54:23 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0805/MagicBall.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task08.task0805;
+
+import java.util.Random;
+
+public class MagicBall {
+    private static final String CERTAIN = "Бесспорно";
+    private static final String DEFINITELY = "Определённо да";
+    private static final String MOST_LIKELY = "Вероятнее всего";
+    private static final String OUTLOOK_GOOD = "Хорошие перспективы";
+    private static final String ASK_AGAIN_LATER = "Спроси позже";
+    private static final String TRY_AGAIN = "Попробуй снова";
+    private static final String NO = "Мой ответ — нет";
+    private static final String VERY_DOUBTFUL = "Весьма сомнительно";
+
+    public static String getPrediction() {
+        //напишите тут ваш код
+        return null;
+    }
+}
