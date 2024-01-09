389a7256c6316c164e1b40f42386e667f1e0a380
DenisSever <sedihdenis@bk.ru>
2024-01-09 18:03:43 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 */
 
 public class Solution {
+
     public static void main(String[] args) {
         String militaryCommissar = ", явитесь в военкомат";
 //        Scanner name = new Scanner(System.in);
