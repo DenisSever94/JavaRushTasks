3f65bd63456af15d855b215ca608519a2a271fb5
DenisSever <sedihdenis@bk.ru>
2024-01-10 11:06:42 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0806/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task08.task0806;
+
+
+public class Solution {
+    public static void main(String[] args) {
+      java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+}
