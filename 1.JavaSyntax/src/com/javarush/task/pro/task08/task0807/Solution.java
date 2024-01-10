63a40da49056015c991133f4e5eda19ece6cafad
DenisSever <sedihdenis@bk.ru>
2024-01-10 11:06:47 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0807/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task08.task0807;
+
+
+/* 
+Импорты: Part 2
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        InputStreamReader inputStreamReader = new InputStreamReader(java.lang.System.in);
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    }
+}
