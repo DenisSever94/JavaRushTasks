9d8e5936e3bda9ae47095689f40fc78a2ff9fd62
DenisSever <sedihdenis@bk.ru>
2024-01-11 22:45:57 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
@@ -3,18 +3,18 @@ package com.javarush.task.pro.task05.task0507;
 import java.util.Scanner;
 
 public class Solution {
-    public static int[] array;
+    public static int[] array1;
 
     public static void main(String[] args) throws Exception {
      Scanner scanner = new Scanner(System.in);
      int size = scanner.nextInt();
-     array = new int[size];
+     array1 = new int[size];
         for (int i = 0; i < size; i++) {
-            array[i] = scanner.nextInt();
-        } int max = array[0];
+            array1[i] = scanner.nextInt();
+        } int max = array1[0];
         for (int i = 0; i < size; i++) {
-            if (array[i] > max)
-                max = array[i];
+            if (array1[i] > max)
+                max = array1[i];
 
         }
         System.out.println(max);
