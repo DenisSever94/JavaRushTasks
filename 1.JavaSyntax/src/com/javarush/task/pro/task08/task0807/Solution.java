f4cd5cb3a6e4c17f704140beb6774f33ea0180c7
DenisSever <sedihdenis@bk.ru>
2024-01-11 22:45:44 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0807/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0807/Solution.java
@@ -6,9 +6,9 @@ package com.javarush.task.pro.task08.task0807;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        LinkedList<String> linkedList = new LinkedList<>();
-        InputStreamReader inputStreamReader = new InputStreamReader(java.lang.System.in);
-        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+//    public static void main(String[] args) {
+//        LinkedList<String> linkedList = new LinkedList<>();
+//        InputStreamReader inputStreamReader = new InputStreamReader(java.lang.System.in);
+//        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
     }
-}
+//}
