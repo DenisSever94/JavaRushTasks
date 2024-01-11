1bfe13e1289ea249b214536de4b87026d4e7e4af
DenisSever <sedihdenis@bk.ru>
2024-01-11 22:46:41 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0809/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task08.task0809;
+
+/* 
+Counter
+
+*/
+public class Solution {
+
+    public static void main(String[] args) throws InterruptedException {
+        printNumbers();
+    }
+
+    public static void printNumbers() throws InterruptedException {
+        //напишите тут ваш код
+    }
+}
