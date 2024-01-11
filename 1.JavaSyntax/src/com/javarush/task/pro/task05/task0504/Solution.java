aa8b5ca84153267f995dccbcda7a4e695ac89df6
DenisSever <sedihdenis@bk.ru>
2024-01-11 22:39:47 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0504/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0504/Solution.java
@@ -4,21 +4,23 @@ package com.javarush.task.pro.task05.task0504;
 Объединяем массивы
 */
 
+import java.util.Arrays;
+
 public class Solution {
-    public static int[] firstArray1 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
     public static int[] resultArray;
 
     public static void main(String[] args) {
 
-        resultArray = new int[firstArray1.length + secondArray.length];
+        resultArray = new int[firstArray.length + secondArray.length];
 
-        for (int q = 0; q < firstArray1.length; q++) {
-            resultArray[q] = firstArray1[q];
+        for (int i = 0; i < firstArray.length; i++) {
+            resultArray[i] = firstArray[i];
 
         }
         for (int e = 0; e < secondArray.length; e++) {
-            resultArray[firstArray1.length + e] = secondArray[e];
+            resultArray[firstArray.length + e] = secondArray[e];
         }
 
         for (int i = 0; i < resultArray.length; i++) {
