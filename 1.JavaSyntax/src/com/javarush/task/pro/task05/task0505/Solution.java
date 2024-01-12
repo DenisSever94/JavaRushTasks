4ace16613782d9921f6e788a2e1a6ebd0dc3f72f
DenisSever <sedihdenis@bk.ru>
2024-01-13 01:49:55 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number1 = scanner.nextInt();
-        int[] array = new int[number1];
+        Scanner scanner66 = new Scanner(System.in);
+        int number = scanner66.nextInt();
+        int[] array = new int[number];
         for (int i = 0; i < array.length; i++) {
-            array[i] = scanner.nextInt();
+            array[i] = scanner66.nextInt();
 
         }
 
-        if (number1 % 2 != 0) {
-            for (int i = 0; i < number1; i++) {
+        if (number % 2 != 0) {
+            for (int i = 0; i < number; i++) {
                 System.out.println(array[i]);
             }
         } else {
