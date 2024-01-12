6b18dfc6897eb80ecd68da01f48678d37d031d78
DenisSever <sedihdenis@bk.ru>
2024-01-12 14:27:18 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0808/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0808/Solution.java
@@ -9,10 +9,15 @@ public class Solution {
     public static void main(String[] args) throws InterruptedException {
         countDown();
     }
+
     public static void countDown() throws InterruptedException {
         System.out.println("3");
+        Thread.sleep(1000);
         System.out.println("2");
+        Thread.sleep(1000);
         System.out.println("1");
+        Thread.sleep(1000);
         System.out.println("Старт!");
+
     }
 }
