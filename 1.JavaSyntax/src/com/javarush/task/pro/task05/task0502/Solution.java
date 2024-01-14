1f45a5be7684b965baa7b86f50c8f9a2de7d7ec0
DenisSever <sedihdenis@bk.ru>
2024-01-14 20:24:56 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0502/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0502/Solution.java
@@ -7,26 +7,26 @@ package com.javarush.task.pro.task05.task0502;
 public class Solution {
     public static final String ODD = "Нечётный";
     public static final String EVEN = "Чётный";
-    public static String[] strings1 = new String[5];
+    public static String[] strings = new String[5];
 
     public static void main(String[] args) {
-        strings1[0] = "Чётный";
-        strings1[1] = "Нечётный";
-        strings1[2] = "Чётный";
-        strings1[3] = "Нечётный";
-        strings1[4] = "Чётный";
+        strings[0] = "Чётный";
+        strings[1] = "Нечётный";
+        strings[2] = "Чётный";
+        strings[3] = "Нечётный";
+        strings[4] = "Чётный";
 
 
 
         System.out.print("index = 0");
-        System.out.println(" value = " + strings1[0]);
+        System.out.println(" value = " + strings[0]);
         System.out.print("index = 1");
-        System.out.println(" value = " + strings1[1]);
+        System.out.println(" value = " + strings[1]);
         System.out.print("index = 2");
-        System.out.println(" value = " + strings1[2]);
+        System.out.println(" value = " + strings[2]);
         System.out.print("index = 3");
-        System.out.println(" value = " + strings1[3]);
+        System.out.println(" value = " + strings[3]);
         System.out.print("index = 4");
-        System.out.println(" value = " + strings1[4]);
+        System.out.println(" value = " + strings[4]);
     }
 }
