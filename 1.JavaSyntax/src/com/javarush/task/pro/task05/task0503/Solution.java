18607f99dd10d035f9b3b2c10f1769979d2b8d3b
DenisSever <sedihdenis@bk.ru>
2024-01-14 20:24:20 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
@@ -10,15 +10,15 @@ public class Solution {
 
     public static void main(String[] args) {
         for (int i = 0; i < array.length; i++) {
-            if ((array[i] % 2) == 0) {
-                array[i] *= -1;
+         if ((array[i] % 2) == 0) {
+             array[i] *= -1;
 
-            }
+         }
+            System.out.println(array[0]);
+            System.out.println(array[1]);
+            System.out.println(array[2]);
+            System.out.println(array[3]);
+            System.out.println(array[4]);
         }
-        System.out.println(array[0]);
-        System.out.println(array[1]);
-        System.out.println(array[2]);
-        System.out.println(array[3]);
-        System.out.println(array[4]);
     }
 }
