cb883f1d3008f2137b086bd4a3892f3c13deaa52
DenisSever <sedihdenis@bk.ru>
2024-01-17 12:29:56 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0909/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task09.task0909;
+
+/* 
+Экранирование символов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\" \nIt's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\"");
+    }
+}
