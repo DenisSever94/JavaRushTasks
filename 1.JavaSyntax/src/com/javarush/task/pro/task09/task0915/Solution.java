da02c6d792bb4d411f7c04ecec64838ff03d903c
DenisSever <sedihdenis@bk.ru>
2024-01-20 18:44:19 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0915/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0915/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.task.pro.task09.task0915;
 
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 /* 
@@ -10,20 +9,17 @@ StringTokenizer
 
 public class Solution {
     public static void main(String[] args) {
-
-        String packagePath = "java.util.stream"; // это строка которая будет делиться
-        String[] tokens = getTokens(packagePath, "\\."); // это массив в который сохраняется строка и его делитель
+        String packagePath = "java.util.stream";
+        String[] tokens = getTokens(packagePath, "\\.");
         System.out.println(Arrays.toString(tokens));
     }
 
     public static String[] getTokens(String query, String delimiter) {
-        StringTokenizer str = new StringTokenizer(query, delimiter); // создает новый объект и в него передается сторка и делитель
-        String[] sr = new String[str.countTokens()]; // создается новый массив для хранения подстрок , размер задается колличеством токенов в строке
-        for (int i = 0; i < sr.length; i++) { // цикл пробегается по всей длине массива и заполняет новый массив подстроками
+        StringTokenizer str = new StringTokenizer(query, delimiter);
+        String[] sr = new String[str.countTokens()];
+        for (int i = 0; i <sr.length; i++) {
             sr[i] = str.nextToken();
         }
         return sr;
-
-
     }
 }
