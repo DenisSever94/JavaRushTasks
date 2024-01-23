94fbb5166a0b33c1d12901975d58729a17a93091
DenisSever <sedihdenis@bk.ru>
2024-01-23 10:51:26 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1011/Car.java
@@ -0,0 +1,46 @@
+package com.javarush.task.pro.task10.task1011;
+
+import java.util.Objects;
+
+
+
+public class Car {
+
+
+    private String model;
+    private int year;
+
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, year);
+    }
+
+
+    public static void main(String[] args) {
+        Car lamborghini2 = new Car("Lamborghini", 2020);
+        Car lamborghini1 = new Car("Lamborghini", 2020);
+        Car ferrari = new Car("Ferrari", 2020);
+        Car ferrari1 = new Car("Ferrari", 2020);
+        Car bugatti = new Car("Bugatti", 2020);
+        Car bugatti1 = new Car("Bugatti", 2020);
+        System.out.println(ferrari.hashCode() == ferrari.hashCode());
+        System.out.println(lamborghini2.hashCode() == lamborghini1.hashCode());
+        System.out.println(ferrari.hashCode() == ferrari1.hashCode());
+        System.out.println(bugatti.hashCode() == bugatti1.hashCode());
+        System.out.println(bugatti.hashCode() == lamborghini2.hashCode());
+        System.out.println(lamborghini2.hashCode() == ferrari.hashCode());
+    }
+}
