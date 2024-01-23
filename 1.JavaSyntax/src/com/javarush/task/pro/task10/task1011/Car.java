4f5828820130c493718f6d27118adb66fe973974
DenisSever <sedihdenis@bk.ru>
2024-01-23 10:51:57 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1011/Car.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1011/Car.java
@@ -30,17 +30,17 @@ public class Car {
 
 
     public static void main(String[] args) {
-        Car lamborghini2 = new Car("Lamborghini", 2020);
+        Car lamborghini = new Car("Lamborghini", 2020);
         Car lamborghini1 = new Car("Lamborghini", 2020);
         Car ferrari = new Car("Ferrari", 2020);
         Car ferrari1 = new Car("Ferrari", 2020);
         Car bugatti = new Car("Bugatti", 2020);
         Car bugatti1 = new Car("Bugatti", 2020);
         System.out.println(ferrari.hashCode() == ferrari.hashCode());
-        System.out.println(lamborghini2.hashCode() == lamborghini1.hashCode());
+        System.out.println(lamborghini.hashCode() == lamborghini1.hashCode());
         System.out.println(ferrari.hashCode() == ferrari1.hashCode());
         System.out.println(bugatti.hashCode() == bugatti1.hashCode());
-        System.out.println(bugatti.hashCode() == lamborghini2.hashCode());
-        System.out.println(lamborghini2.hashCode() == ferrari.hashCode());
+        System.out.println(bugatti.hashCode() == lamborghini.hashCode());
+        System.out.println(lamborghini.hashCode() == ferrari.hashCode());
     }
 }
