f903fedb32f8a8cbb2eca5a1de603702b2bd6459
DenisSever <sedihdenis@bk.ru>
2024-01-26 20:10:19 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1017;
+
+
+public class Earth {
+    public static void main(String[] args) {
+
+        new Eurasia(545);
+        new NorthAmerica(5445);
+        new SouthAmerica(443);
+        new Africa(434);
+        new Antarctica(4344);
+        new Australia(55544);
+    }
+}
