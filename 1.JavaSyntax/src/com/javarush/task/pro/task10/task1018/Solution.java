77d9e5e1da06b34bf0aa8f8a1f35dd94a9176ace
DenisSever <sedihdenis@bk.ru>
2024-01-28 19:20:45 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1018/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1018/Solution.java
@@ -7,10 +7,10 @@ package com.javarush.task.pro.task10.task1018;
 public class Solution {
 
     public static void main(String[] args) {
-        Coordinate coordinate = new Coordinate();
-        coordinate.setX(15);
-        coordinate.setY(25);
-        System.out.println(coordinate.getX());
-        System.out.println(coordinate.getY());
+        Coordinate coordinate1 = new Coordinate();
+        coordinate1.setX(15);
+        coordinate1.setY(25);
+        System.out.println(coordinate1.getX());
+        System.out.println(coordinate1.getY());
     }
 }
