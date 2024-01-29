cc65a2db8114652ce60af106f001782dfc120796
DenisSever <sedihdenis@bk.ru>
2024-01-29 14:52:28 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -6,7 +6,10 @@ package com.javarush.task.pro.task10.task1006;
 
 public class Solution {
     public static void main(String[] args) {
-       new ElectricCar();
+//        new Car("ElectricCar");
+//        new Car("GasCar");
+//        new Car("HybridCar");
+        new ElectricCar();
         new GasCar();
         new HybridCar();
     }
