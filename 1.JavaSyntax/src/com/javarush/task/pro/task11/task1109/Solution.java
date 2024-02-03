d7524630327912a6719e9044fb05cce51346be5d
DenisSever <sedihdenis@bk.ru>
2024-02-03 18:07:44 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1109/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task11.task1109;
+
+/* 
+Объекты внутренних и вложенных классов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
