4f586708b532a231cfe85c77b086b632ef9bc2ac
DenisSever <sedihdenis@bk.ru>
2024-02-07 23:01:23 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1126/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task11.task1126;
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+        Student student = new Student("Амиго");
+
+        System.out.println(student.name);
+    }
+}
