8ea0906f5e163e0a054b92bb2d5688b401158bbe
DenisSever <sedihdenis@bk.ru>
2024-02-13 23:32:00 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1211/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task12.task1211;
+import java.util.Arrays;
+public class Solution {
+    public static int[] numbers = new int[]{56, 45, 89, 1, 45, 13, 4, 69, 11, -89};
+    public static void main(String[] args) {
+        print();
+        sort();
+        System.out.println("\nОтсортированные числа в натуральном порядке:");
+        print();
+    }
+
+    public static void sort() {
+        Arrays.sort(numbers);
+
+    }
+
+    public static void print() {
+        for (int number : numbers) {
+            System.out.print(number + "\t");
+        }
+    }
+}
