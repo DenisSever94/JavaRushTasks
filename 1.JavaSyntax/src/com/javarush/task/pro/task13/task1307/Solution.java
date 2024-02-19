bb1495464b381ed99cd0550749f6af4c91242244
DenisSever <sedihdenis@bk.ru>
2024-02-20 01:07:56 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1307/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task13.task1307;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+
+
+public class Solution {
+
+    public static void reverse(ArrayList<Integer> list) {
+        Collections.reverse(list);
+    }
+
+    public static void sort(ArrayList<Integer> list) {
+        Collections.sort(list);
+    }
+
+    public static void rotate(ArrayList<Integer> list, int distance) {
+        Collections.rotate(list, distance);
+    }
+
+    public static void shuffle(ArrayList<Integer> list) {
+        Collections.shuffle(list);
+    }
+}
