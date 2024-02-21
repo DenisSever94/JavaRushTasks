d7c2560a2b5d12b987b8bf7e4de751499136ed9d
DenisSever <sedihdenis@bk.ru>
2024-02-22 01:14:27 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task13.task1316;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        JavarushQuest[] quests = JavarushQuest.values();
+        for (JavarushQuest java : quests) {
+            System.out.println(java.ordinal());
+
+
+        }
+
+
+    }
+}
