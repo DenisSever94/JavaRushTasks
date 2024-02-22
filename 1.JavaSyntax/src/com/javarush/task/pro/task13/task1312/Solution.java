dad255d755f6178189f486eda9f5efb6d094e170
DenisSever <sedihdenis@bk.ru>
2024-02-23 00:01:46 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1312/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1312/Solution.java
@@ -2,29 +2,25 @@ package com.javarush.task.pro.task13.task1312;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
-/* 
-ArrayList vs HashMap
-*/
-
+import java.util.Map;
 public class Solution {
 
     public static void main(String[] args) {
         System.out.println(getProgrammingLanguages());
     }
 
-    public static ArrayList<String> getProgrammingLanguages() {
+    public static  HashMap<Integer, String> getProgrammingLanguages() {
+        HashMap<Integer, String>  programmingLanguages = new HashMap<Integer, String>();
+         programmingLanguages.put(0, "Java");
+         programmingLanguages.put(1, "Kotlin");
+         programmingLanguages.put(2, "Go");
+         programmingLanguages.put(3, "Javascript");
+         programmingLanguages.put(4, "Typescript");
+         programmingLanguages.put(5, "Python");
+         programmingLanguages.put(6, "PHP");
+         programmingLanguages.put(7, "C++");
+            return  programmingLanguages;
 
-        ArrayList<String> programmingLanguages = new ArrayList<>();
-        programmingLanguages.add("Java");
-        programmingLanguages.add("Kotlin");
-        programmingLanguages.add("Go");
-        programmingLanguages.add("Javascript");
-        programmingLanguages.add("Typescript");
-        programmingLanguages.add("Python");
-        programmingLanguages.add("PHP");
-        programmingLanguages.add("C++");
-        return programmingLanguages;
     }
 
 }
