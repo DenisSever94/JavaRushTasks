c479566c1c4d83b77268bbb73dfb21c805cc536c
DenisSever <sedihdenis@bk.ru>
2024-02-22 23:59:43 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1315/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task13.task1315;
+
+/* 
+Четыре сезона
+*/
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+       Season season = Season.WINTER;
+        Season season1 = Season.SPRING;
+        Season season2 = Season.AUTUMN;
+        Season season3 = Season.SUMMER;
+        System.out.println(season);
+        System.out.println(season1);
+        System.out.println(season2);
+        System.out.println(season3);
+    }
+}
