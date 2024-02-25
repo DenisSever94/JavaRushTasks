4300da09ba5fa03db1f66fdbe11c2cfa14daafc3
DenisSever <sedihdenis@bk.ru>
2024-02-26 00:43:53 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1009/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1009/Solution.java
@@ -7,13 +7,13 @@ package com.javarush.task.pro.task10.task1009;
 public class Solution {
 
     public static void main(String[] args) {
-        String s1 = new String("JavaRush");
-        s1 = "JavaRush";
+        String s = new String("JavaRush");
+        s = "JavaRush";
         String s2 = "JavaRush";
 //        s2 = new String("JavaRush");
         String s3 = "JavaRush";
 //        s3 = new String("JavaRush");
-        System.out.println(s1 == s3);
+        System.out.println(s == s3);
         System.out.println(s2.equals(s3));
     }
 }
