88c4f142586ce7cdfe6bb2c8b2a39fbe516579ac
DenisSever <sedihdenis@bk.ru>
2024-02-28 00:56:17 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0914/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0914/Solution.java
@@ -6,15 +6,17 @@ package com.javarush.task.pro.task09.task0914;
 
 public class Solution {
     public static void main(String[] args) {
-        String path = "/usr/java/jdk1.8/bin/";
+        String path1 = "/usr/java/jdk1.8/bin/";
 
         String jdk13 = "jdk-13";
-        System.out.println(changePath(path, jdk13));
+        System.out.println(changePath(path1, jdk13));
     }
 
     public static String changePath(String path, String jdk) {
+        int firstIndex = path.indexOf("jdk");
+        int lastIndex = path.indexOf("/", firstIndex);
+        String oldJdk = path.substring(firstIndex, lastIndex);
 
-        return path.replace("jdk1.8", jdk);
-
+        return path.replace(oldJdk, jdk);
     }
 }
