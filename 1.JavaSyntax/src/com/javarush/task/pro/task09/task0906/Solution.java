f8adc9b9510add6cd25a437d62ae5b45cd6e4b56
DenisSever <sedihdenis@bk.ru>
2024-03-02 08:10:37 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0906/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0906/Solution.java
@@ -2,59 +2,43 @@ package com.javarush.task.pro.task09.task0906;
 
 import java.util.regex.Pattern;
 
-/* 
-Двоичный конвертер
-*/
-import java.util.Scanner;
-
 public class Solution {
     public static void main(String[] args) {
+        int decimalNumber1 = Integer.MAX_VALUE;
+        System.out.println("Десятичное число " + decimalNumber1 + " равно двоичному числу " + toBinary(decimalNumber1));
+        String binaryNumber = "1111111111111111111111111111111";
+        System.out.println("Двоичное число " + binaryNumber + " равно десятичному числу " + toDecimal(binaryNumber));
+    }
 
+    public static String toBinary(int decimalNumber) {
+        if (decimalNumber <= 0) {
+            return "";
+        } else {
+            String bi = "";
+            while (decimalNumber != 0) {
+                int num = decimalNumber % 2;
+                bi = num + bi;
+                decimalNumber = decimalNumber / 2;
 
-        String militaryCommissar = ", явитесь в военкомат";
+            }
+            return bi;
+        }
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        Scanner scanner1 = new Scanner (System.in);
-        int age = scanner1.nextInt();
-        if (age >= 18 && age <= 28) {
-            System.out.println(name + militaryCommissar);
+    public static int toDecimal(String binaryNumber) {
+        int decimalNumber = 0;
+        if (binaryNumber == null) {
+            return decimalNumber;
         }
 
 
-//        int decimalNumber = Integer.MAX_VALUE;
-//        System.out.println("Десятичное число " + decimalNumber + " равно двоичному числу " + toBinary(decimalNumber));
-//        String binaryNumber = "1111111111111111111111111111111";
-//        System.out.println("Двоичное число " + binaryNumber + " равно десятичному числу " + toDecimal(binaryNumber));
+
+        for (int i = 0; i < binaryNumber.length(); i++) {
+            int index = binaryNumber.length() - 1 - i;
+            int value = Character.getNumericValue(binaryNumber.charAt(index));
+           decimalNumber += value * Math.pow(2, i);
+
+        }
+        return decimalNumber;
     }
-//
-//    public static String toBinary(int decimalNumber) {
-////        if (decimalNumber <= 0) {
-//            return "";
-//        } else {
-//            String bi = "";
-//            while (decimalNumber != 0) {
-//                int num = decimalNumber % 2;
-//                bi = num + bi;
-//                decimalNumber = decimalNumber / 2;
-//
-//            }
-//            return bi;
-//        }
-//    }
-//
-//    public static int toDecimal(String binaryNumber) {
-//        int decimalNumber = 0;
-//        if (binaryNumber == null) {
-//            return decimalNumber;
-//        }
-//
-//        for (int i = 0; i < binaryNumber.length(); i++) {
-//            int index = binaryNumber.length() - 1 - i;
-//            int value = Character.getNumericValue(binaryNumber.charAt(index));
-//            decimalNumber += value * Math.pow(2, i);
-//
-//        }
-//        return decimalNumber;
-//    }
 }
