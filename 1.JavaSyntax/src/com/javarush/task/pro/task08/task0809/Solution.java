292b0547d478eccef5ed89087cf505768dd780ae
DenisSever <sedihdenis@bk.ru>
2024-03-07 02:00:18 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0809/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0809/Solution.java
@@ -1,19 +1,15 @@
 package com.javarush.task.pro.task08.task0809;
 
-/* 
-Counter
-
-*/
 public class Solution {
 
     public static void main(String[] args) throws InterruptedException {
-        printNumbers1();
+        printNumbers();
 
 
     }
 
 
-    public static void printNumbers1() throws InterruptedException {
+    public static void printNumbers() throws InterruptedException {
 
         for (int i = 1; i < 11; i++) {
             System.out.println(i);
