94263af852850635f0deda1a9a010f348f4d4d91
DenisSever <sedihdenis@bk.ru>
2024-03-06 06:24:37 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0603/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0603/Solution.java
@@ -7,10 +7,10 @@ package com.javarush.task.pro.task06.task0603;
 public class Solution {
 
     public static void main(String[] args) {
-        String firstname = "Ольга";
-        String lastname = "Киприяновна";
-        String favouritedish = "Пельмени";
-        printPersonInfo(firstname, lastname, favouritedish);
+        String firstNamee = "Ольга";
+        String lastName = "Киприяновна";
+        String favouriteDish = "Пельмени";
+        printPersonInfo(firstNamee, lastName, favouriteDish);
     }
 
     public static void printPersonInfo(String name, String surname, String meal){
