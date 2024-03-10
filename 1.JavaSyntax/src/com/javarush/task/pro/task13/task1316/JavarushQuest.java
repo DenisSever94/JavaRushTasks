b4473023699791a2dd3d554a617742b99bf3828f
DenisSever <sedihdenis@bk.ru>
2024-03-10 03:35:14 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/JavarushQuest.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task13.task1316;
+
+
+public enum JavarushQuest {
+    JAVA_SYNTAX,
+    JAVA_CORE,
+    JAVA_MULTITHREADING,
+    JAVA_COLLECTIONS,
+    CS_50,
+    ANDROID,
+    GAMES
+}
