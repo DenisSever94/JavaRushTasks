354acab12a64fc26408eadc3de6e37168c423d61
DenisSever <sedihdenis@bk.ru>
2024-03-11 02:56:31 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/ElectricCar.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/ElectricCar.java
@@ -1,6 +1,6 @@
 package com.javarush.task.pro.task10.task1006;
 
-public class ElectricCar extends Car1 {
+public class ElectricCar extends Car {
 
     public ElectricCar() {
         super("ElectricCar");
