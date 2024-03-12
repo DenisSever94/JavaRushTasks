a97acf0d207b514ee459794658804edf4b40a3d2
DenisSever <sedihdenis@bk.ru>
2024-03-12 23:06:32 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0615/Earth.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0615/Earth.java
@@ -5,8 +5,8 @@ package com.javarush.task.pro.task06.task0615;
 */
 
 public class Earth {
-    String name = "Земля";
-    double square = 510_100_000;
-    long population = 7_594_000_000L;
-    long equatorLength = 40_075_696;
+    public static final String NAME1 = "Земля";
+    public static final double SQUARE = 510_100_000;
+    public static final long POPULATION = 7_594_000_000L;
+    public static final long EQUATOR_LENGTH = 40_075_696;
 }
