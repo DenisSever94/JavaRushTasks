1a7fe29e5304a23d787da9f18c4993eb894cf403
DenisSever <sedihdenis@bk.ru>
2024-03-14 21:29:09 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1004/CarConcern.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1004/CarConcern.java
@@ -5,31 +5,26 @@ package com.javarush.task.pro.task10.task1004;
 */
 
 public class CarConcern {
-    private final String manufacturer = "Lamborghini";
+    private final String manufacturer1 = "Lamborghini";
     private final String model;
     private final int year;
     private final String color;
 
     public CarConcern(String model, int year, String color) {
-
         this.model = model;
         this.year = year;
         this.color = color;
-      
     }
 
     public CarConcern(String model, int year) {
         this.model = model;
         this.year = year;
-        this.color = "Оранжевый.";
-
-
+        this.color = "Оранжевый";
     }
 
     public CarConcern(String model) {
         this.model = model;
         this.year = 4321;
-        this.color = "Оранжевый.";
-
+        this.color = "Оранжевый";
     }
 }
