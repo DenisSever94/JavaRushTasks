2d5169c7cbac70b3d655ee2bdd43b53fb64b5894
DenisSever <sedihdenis@bk.ru>
2024-03-16 01:13:03 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0209/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0209/Solution.java
@@ -7,11 +7,11 @@ package com.javarush.task.pro.task02.task0209;
 public class Solution {
 
     public static void main(String[] args) {
-        int x = 2;
+        int x1 = 2;
         int y = 4;
         int z = 0;
 
-        String digits = x + y +"" + z; //напишите тут ваш код
+        String digits = ""; //напишите тут ваш код
 
         System.out.println(digits);
     }
