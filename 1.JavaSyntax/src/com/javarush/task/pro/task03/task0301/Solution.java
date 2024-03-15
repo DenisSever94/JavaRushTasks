3228c5f29e7eda09a8ee7cf9935bc7ba7c51fe53
DenisSever <sedihdenis@bk.ru>
2024-03-16 01:13:12 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
@@ -2,22 +2,21 @@ package com.javarush.task.pro.task03.task0301;
 
 import java.util.Scanner;
 
+/* 
+Тепло или холодно
+*/
 
 public class Solution {
     public static void main(String[] args) {
-
         String cold1 = "на улице холодно";
         String warm = "на улице тепло";
-
-        Scanner scanner = new Scanner(System.in);
-
-        int tempStreet = scanner.nextInt();
-        if (tempStreet < 0) {
-            System.out.println(cold1);
-        } else if (tempStreet >= 0) {
-            System.out.println(warm);
-        }
-
-
+        Scanner pogoda = new Scanner (System.in);
+        int gradus = pogoda.nextInt();
+        if (gradus < 0)
+        System.out.println("на улице холодно");
+        else 
+        System.out.println("на улице тепло");
+        
+        //напишите тут ваш код
     }
 }
