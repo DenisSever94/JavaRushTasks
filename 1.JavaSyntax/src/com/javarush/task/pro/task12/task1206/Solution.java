16569183565c795e8df196a3403248225fa2bfa4
DenisSever <sedihdenis@bk.ru>
2024-03-17 02:03:24 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1206/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1206/Solution.java
@@ -6,12 +6,12 @@ package com.javarush.task.pro.task12.task1206;
 
 public class Solution {
     public static void main(String[] args) {
-        String string = "Думаю, это будет новой фичей." +
+        String string1 = "Думаю, это будет новой фичей." +
                 "Только не говорите никому, что она возникла случайно.";
 
-        System.out.println("Количество цифр в строке : " + countDigits(string));
-        System.out.println("Количество букв в строке : " + countLetters(string));
-        System.out.println("Количество пробелов в строке : " + countSpaces(string));
+        System.out.println("Количество цифр в строке : " + countDigits(string1));
+        System.out.println("Количество букв в строке : " + countLetters(string1));
+        System.out.println("Количество пробелов в строке : " + countSpaces(string1));
     }
 
     public static int countDigits(String string) {
