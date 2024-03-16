649b6df40536be401b29709c6d62630d576dfbcc
DenisSever <sedihdenis@bk.ru>
2024-03-17 02:01:47 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0704/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0704/Solution.java
@@ -5,7 +5,7 @@ package com.javarush.task.pro.task07.task0704;
 */
 
 public class Solution {
-    double earthDiameter = 1.2742e+4;
+    double earthDiameter1 = 1.2742e+4;
     double lightSpeed = 2.99792458e+8;
     double uraniumAtomicMass = 2.380289e+2;
     double averageBeeWeight = 8.5e-2;
