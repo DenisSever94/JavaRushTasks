6b03e7e3ed4d103e6e70568fa644acbe16664c56
DenisSever <sedihdenis@bk.ru>
2024-03-23 18:09:37 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1502/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task15.task1502;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String line = scanner.nextLine();
+            System.out.println(line.toUpperCase());
+        } catch (Exception e) {
+            System.out.println("Something went wrong : " + e);
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+}
