4103917ca5cedc01ae91954901e7c90b0c8f2008
DenisSever <sedihdenis@bk.ru>
2024-03-24 13:56:43 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task15.task1504;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+
+//        Files.newInputStream()
+
+    }
+}
+
