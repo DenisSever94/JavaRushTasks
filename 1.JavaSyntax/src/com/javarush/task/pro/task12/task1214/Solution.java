ccb06ca3db2dea5ff53aa5bef37dd55e1442fee6
DenisSever <sedihdenis@bk.ru>
2024-03-26 01:53:06 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1214/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1214/Solution.java
@@ -3,15 +3,12 @@ package com.javarush.task.pro.task12.task1214;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/* 
-Прощай, Паскаль
-*/
 
 public class Solution {
-    public static ArrayList<String> programmingLanguages =
+    public static ArrayList<String> arrayList =
             new ArrayList<>(Arrays.asList("C", "C++", "Python", "JavaScript", "Ruby", "Java", "Pascal"));
 
     public static void main(String[] args) {
-  programmingLanguages.remove(programmingLanguages.indexOf("Pascal"));
+  arrayList.remove(arrayList.indexOf("Pascal"));
     }
 }
