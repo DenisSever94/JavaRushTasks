da194ccb490627203fd493d54ed61063a2418b67
DenisSever <sedihdenis@bk.ru>
2024-03-27 21:31:19 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1103/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1103/Solution.java
@@ -1,15 +1,12 @@
 package com.javarush.task.pro.task11.task1103;
 
-/* 
-Презентация роботов
-*/
 
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Презентация роботов произведенных компанией " + Robot.vendor);
 
-        Robot robot1 = new Robot("RCV-322", 2019);
-        robot1.displayInformation();
+        Robot robot = new Robot("RCV-322", 2019);
+        robot.displayInformation();
 
         Robot robot2 = new Robot("JavaTron-008", 2017);
         robot2.displayInformation();
