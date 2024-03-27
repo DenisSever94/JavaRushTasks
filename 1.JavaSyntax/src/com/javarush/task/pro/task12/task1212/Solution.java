909d958dec8972fbd1e830a22c63b2f315c5b80b
DenisSever <sedihdenis@bk.ru>
2024-03-27 21:31:09 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1212/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1212/Solution.java
@@ -3,9 +3,9 @@ package com.javarush.task.pro.task12.task1212;
 public class Solution {
 
     public static void main(String[] args) {
-        CustomStringArrayList arrayList = new CustomStringArrayList();
+        CustomStringArrayList customStringArrayList = new CustomStringArrayList();
         for(int i = 0; i < 21; i++) {
-            arrayList.add("count" + i);
+            customStringArrayList.add("count" + i);
         }
     }
 }
