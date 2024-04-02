c411999fc7227e3bd23d3bcebe3a8cf6f0cbd59d
DenisSever <sedihdenis@bk.ru>
2024-04-03 00:43:54 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
@@ -1,18 +1,37 @@
 package com.javarush.task.pro.task15.task1504;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        String string1 = scanner.nextLine();
-        String string2 = scanner.nextLine();
+        String path1 = scanner.nextLine();
+        String path2 = scanner.nextLine();
+
+        try (InputStream inputStream = Files.newInputStream(Paths.get(path1));
+             OutputStream outputStream = Files.newOutputStream(Paths.get(path2))) {
+
+            byte[] bytesIn = inputStream.readAllBytes();
+            byte[] bytesOut = new byte[bytesIn.length];
+            for (int i = 0; i < bytesIn.length; i++) {
+
+
+            }
+
+
+        }
+
 
     }
+
+
 }
+
+
+
 
