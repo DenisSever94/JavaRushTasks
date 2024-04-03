91ae3a22b9c010782e1a48911a5f20be6931d6e4
DenisSever <sedihdenis@bk.ru>
2024-04-04 01:57:57 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1109/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1109/Solution.java
@@ -1,13 +1,11 @@
 package com.javarush.task.pro.task11.task1109;
 
-/* 
-Объекты внутренних и вложенных классов
-*/
+
 
 public class Solution {
     public static void main(String[] args) {
-        Outer outer = new Outer();
-        Outer.Inner inner = outer.new Inner();
+        Outer outer1 = new Outer();
+        Outer.Inner inner = outer1.new Inner();
         Outer.Nested nested = new Outer.Nested();
 
     }
