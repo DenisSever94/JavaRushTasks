86e81f289d724fe02c2168c46c6c5747c6159b84
DenisSever <sedihdenis@bk.ru>
2024-04-05 02:06:44 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
@@ -1,13 +1,12 @@
 package com.javarush.task.pro.task02.task0203;
 
-
 public class Solution {
 
     public static void main(String[] args) {
-        int number = 546;
+        int i = 546;
 
-        int lastDigit = number % 10;
+        int lastDigit; //напишите тут ваш код
 
-        System.out.println(lastDigit);
+//        System.out.println(lastDigit);
     }
 }
