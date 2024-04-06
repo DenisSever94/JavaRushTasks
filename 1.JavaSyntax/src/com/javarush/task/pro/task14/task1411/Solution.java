952e99275703c7b582be5db3e68850d1cfa7cec6
DenisSever <sedihdenis@bk.ru>
2024-04-06 22:25:12 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task14/task1411/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1411/Solution.java
@@ -2,17 +2,22 @@ package com.javarush.task.pro.task14.task1411;
 
 import java.io.FileNotFoundException;
 import java.nio.file.FileSystemException;
+
+/* 
+Распаковка исключений
+*/
+
 public class Solution {
 
     public static final String FAILED_TO_READ = "Не удалось прочесть файл.";
     public static final String FAILED_TO_WRITE = "Не удалось записать в файл.";
 
-    public static FileManager fileManager = new FileManager();
+    public static FileManager filemanager = new FileManager();
 
     public static void main(String[] args) {
         try {
-            fileManager.copyFile("book.txt", "book_final_copy.txt");
-            fileManager.copyFile("book_final_copy.txt", "book_last_copy.txt");
+            filemanager.copyFile("book.txt", "book_final_copy.txt");
+            filemanager.copyFile("book_final_copy.txt", "book_last_copy.txt");
             throw new RuntimeException();
         } catch (RuntimeException runtimeException) {
             Throwable throwable = runtimeException.getCause();
