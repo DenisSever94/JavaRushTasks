e8cba53fc708916719287cce4537028fd1be4486
DenisSever <sedihdenis@bk.ru>
2024-04-10 12:17:07 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1521/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task15.task1521;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        URL url = new URL(line);
+        InputStream inputStream = url.openStream();
+        byte[] buffer = inputStream.readAllBytes();
+        Path path = Files.createTempFile(null, null);
+        Files.write(path, buffer);
+
+    }
+}
