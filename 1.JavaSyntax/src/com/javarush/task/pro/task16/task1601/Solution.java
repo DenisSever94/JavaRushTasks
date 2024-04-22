e5b761ec0fffc93fe6e3debb9b1df9406cdd7e93
DenisSever <sedihdenis@bk.ru>
2024-04-23 00:09:26 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1601/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.pro.task16.task1601;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/* 
+Лишь бы не в понедельник :)
+*/
+
+public class Solution {
+
+    static Date birthDate = new Date(94, Calendar.MARCH, 5);
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(Date date) {
+        String rezult = "";
+        int day = date.getDay();
+        switch (day) {
+            case 0:
+                return "Воскресенье";
+            case 1:
+                return "Понедельник";
+            case 2:
+                return "Вторник";
+            case 3:
+                return "Среда";
+            case 4:
+                return "Четверг";
+            case 5:
+                return "Пятница";
+            case 6:
+                return "Суббота";
+        }
+
+
+        return rezult;
+    }
+}
