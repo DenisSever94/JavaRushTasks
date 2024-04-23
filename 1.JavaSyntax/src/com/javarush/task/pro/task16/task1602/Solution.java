a3586c52d95048369621b8c3cf0d9ff5eefc3dd9
DenisSever <sedihdenis@bk.ru>
2024-04-24 02:04:21 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task16/task1602/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1602/Solution.java
@@ -11,19 +11,19 @@ import java.util.List;
 public class Solution {
 
     public static void main(String[] args) {
-        List<Date> dateList = new ArrayList<>();
-        dateList.add(new Date(2015, 12, 25, 20, 40));
-        dateList.add(new Date("July 20, 1969"));
-        dateList.add(new Date(1989, 11, 9));
-        dateList.add(new Date("January 1, 2000"));
+        List<Date> dates = new ArrayList<>();
+        dates.add(new Date(2015, 12, 25, 20, 40));
+        dates.add(new Date("July 20, 1969"));
+        dates.add(new Date(1989, 11, 9));
+        dates.add(new Date("January 1, 2000"));
 
         System.out.println("before fixes:");
-        dateList.forEach(System.out::println);
+        dates.forEach(System.out::println);
 
-        fixDate(dateList);
+        fixDate(dates);
 
         System.out.println("after fixes:");
-        dateList.forEach(System.out::println);
+        dates.forEach(System.out::println);
     }
 
     static void fixDate(List<Date> brokenDates) {
