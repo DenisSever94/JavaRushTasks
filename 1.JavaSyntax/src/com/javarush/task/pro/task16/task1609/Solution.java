3ba00ba49ea861e5b32fa1deba0c52c9c122224b
DenisSever <sedihdenis@bk.ru>
2024-04-24 02:06:15 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task16/task1609/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1609/Solution.java
@@ -9,9 +9,9 @@ import java.time.LocalDate;
 public class Solution {
 
     public static void main(String[] args) {
-        LocalDate jubilee40Years = LocalDate.of(2010, 3, 13);
+        LocalDate localDate = LocalDate.of(2010, 3, 13);
 
-        LocalDate jubilee30Years = jubilee40Years.minusYears(10);
+        LocalDate jubilee30Years = localDate.minusYears(10);
 
         System.out.println(Math.abs(jubilee30Years.getYear()));
     }
