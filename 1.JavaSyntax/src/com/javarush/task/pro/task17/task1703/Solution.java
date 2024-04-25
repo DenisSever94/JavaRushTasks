93528eadb3b8265f8546c5cac8683a6fe5704b54
DenisSever <sedihdenis@bk.ru>
2024-04-25 16:35:23 +0300

    add solution javaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1703/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task17.task1703;
+
+import java.util.ArrayList;
+
+
+public class Solution {
+    public static ArrayList<Astronaut> astronauts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        createCrew();
+        printCrewInfo();
+    }
+
+    public static void createCrew(){
+
+
+    }
+
+    public static void printCrewInfo() {
+        System.out.println("На борт погружены члены экипажа: ");
+        for (Astronaut astronaut : astronauts) {
+            System.out.println(astronaut.getInfo());
+        }
+    }
+}
