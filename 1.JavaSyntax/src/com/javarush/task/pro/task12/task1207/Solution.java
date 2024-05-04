893dc66529fbc38834e9e80112953471fd3a24cd
DenisSever <sedihdenis@bk.ru>
2024-05-05 01:14:01 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1207/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1207/Solution.java
@@ -6,10 +6,10 @@ package com.javarush.task.pro.task12.task1207;
 
 public class Solution {
     public static void main(String[] args) {
-        int first = 2;
+        int i = 2;
         int second = 3;
 
-        Boolean isLess = isLess(first, second);
+        Boolean isLess = isLess(i, second);
 
         if (isLess) {
             System.out.println("Первое число меньше второго");
