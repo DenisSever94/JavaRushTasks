0083e1de5358bfc49d90c7e72a6b9ccfa79ddb03
DenisSever <sedihdenis@bk.ru>
2024-05-05 01:14:08 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1203/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1203/Solution.java
@@ -5,13 +5,13 @@ package com.javarush.task.pro.task12.task1203;
 */
 
 public class Solution {
-    static Integer first = 1000;
+    static Integer integer = 1000;
     static Integer second = 1000;
     static int third = 1000;
 
     public static void main(String[] args) {
-        System.out.println(first.equals(second));
+        System.out.println(integer.equals(second));
         System.out.println(third == second);
-        System.out.println(third == first);
+        System.out.println(third == integer);
     }
 }
