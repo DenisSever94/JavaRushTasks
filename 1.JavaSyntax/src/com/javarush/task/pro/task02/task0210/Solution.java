648532dcdfc58bb30bc31cdc61884cb539bb4a34
DenisSever <sedihdenis@bk.ru>
2024-05-11 15:46:34 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0210/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0210/Solution.java
@@ -1,9 +1,13 @@
 package com.javarush.task.pro.task02.task0210;
 
+/* 
+С каждым разом огромнее
+*/
+
 public class Solution {
 
     public static void main(String[] args) {
-        String string = "500";
+        String bigAmount = "500";
         String greatAmount = "100000";
 
         int hugeAmount = 0; //напишите тут ваш код
