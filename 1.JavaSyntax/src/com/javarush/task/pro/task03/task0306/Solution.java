3b1704f04bde386130adce9141a22c43771aa828
DenisSever <sedihdenis@bk.ru>
2024-05-11 15:46:30 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0306/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0306/Solution.java
@@ -11,16 +11,6 @@ public class Solution {
     private static final String TRIANGLE_NOT_EXISTS = "треугольник не существует";
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        int side1 = scanner.nextInt();
-        int side2 = scanner.nextInt();
-        int side3 = scanner.nextInt();
-
-        if ((side1 == side2) || (side1 == side3) || (side2 == side3)) {
-            System.out.println(TRIANGLE_EXISTS);
-        } else System.out.println(TRIANGLE_NOT_EXISTS);
-
-
+        //напишите тут ваш код
     }
 }
