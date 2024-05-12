2c12fced1a6d0b54a5f4125308e50fed2fd66b36
DenisSever <sedihdenis@bk.ru>
2024-05-12 14:04:28 +0300

    add solution javaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1111/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task11.task1111;
+
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+
+    public class Adam {
+
+    }
+
+
+    public class Eve extends Adam {
+
+    }
+}
