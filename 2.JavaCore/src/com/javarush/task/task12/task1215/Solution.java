92519a8605c9b9af7f445d58a9ac8c0e57f3b17f
DenisSever <sedihdenis@bk.ru>
2024-05-14 00:53:50 +0300

    add solution javaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1215/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task12.task1215;
+
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public abstract String getName();
+
+        public abstract Pet getChild();
+    }
+
+    public static class Cat extends Pet{
+        @Override
+        public String getName() {
+            return null;
+        }
+
+        @Override
+        public Pet getChild() {
+            return null;
+        }
+    }
+
+    public static class Dog extends Pet {
+
+        @Override
+        public String getName() {
+            return null;
+        }
+
+        @Override
+        public Pet getChild() {
+            return null;
+        }
+    }
+
+}
