53d65f605c952d7b43f35a428f6e257e93002f3e
DenisSever <sedihdenis@bk.ru>
2024-05-15 01:47:12 +0300

    add solution javaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1217;
+
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    interface CanFly {
+        String air();
+    }
+
+    interface CanRun {
+        String run();
+    }
+
+    interface CanSwim {
+        String swim();
+    }
+
+
+}
