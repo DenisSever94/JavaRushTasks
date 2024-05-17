42912ab0117a3ab2f1194e0f2c4ff22e8714a90b
DenisSever <sedihdenis@bk.ru>
2024-05-17 21:56:39 +0300

    add solution javaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1209;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int x, int y) {
+
+        if (x < y) {
+            return x;
+        } else return y;
+
+    }
+
+    public static double min(double x, double y) {
+        if (x < y) {
+            return x;
+        } else return y;
+    }
+
+    public static long min(long x, long y) {
+        if (x < y) {
+            return x;
+        } else return y;
+    }
+}
