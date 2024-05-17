f9c155942423b471f17346ae4645b91bbdd43ab5
DenisSever <sedihdenis@bk.ru>
2024-05-17 21:56:32 +0300

    add solution javaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task12.task1208;
+
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static void print(String s) {
+
+    }
+
+    static void print(int i) {
+
+    }
+
+    static void print(byte b) {
+
+    }
+
+    static void print(long l) {
+
+    }
+
+    static void print(double d) {
+
+    }
+}
