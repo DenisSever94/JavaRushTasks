5d2780ad37fe92cacaa683d558ca98b20459c06c
DenisSever <sedihdenis@bk.ru>
2024-05-19 08:10:17 +0300

    add solution javaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task12.task1210;
+
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int max(int x, int y) {
+        if (x > y) {
+            return x;
+        } else return y;
+    }
+
+    public static double max(double x, double y) {
+        if (x > y) {
+            return x;
+        } else return y;
+    }
+
+    public static long max(long x, long y) {
+        if (x > y) {
+            return x;
+        } else return y;
+    }
+}
