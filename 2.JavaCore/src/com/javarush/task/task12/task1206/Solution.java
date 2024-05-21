022db8736c9d2efeca695349f55a5ed50efbc604
DenisSever <sedihdenis@bk.ru>
2024-05-21 14:30:16 +0300

    add solution javaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1206;
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static int print(int a) {
+
+        return a;
+    }
+
+    static String print(String a) {
+
+        return a;
+    }
+}
