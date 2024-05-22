c793425ee19e26b4d4ba886fa5b64bd4668f7705
DenisSever <sedihdenis@bk.ru>
2024-05-23 00:56:51 +0300

    add solution javaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(4);
+        print((Integer) 3);
+
+
+    }
+
+    static void print(int i) {
+
+    }
+
+    static void print(Integer integer) {
+
+    }
+}
