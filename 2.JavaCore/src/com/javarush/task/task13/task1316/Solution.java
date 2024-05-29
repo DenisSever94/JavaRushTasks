3f31db1eb9333c635cfb9c20ba05185e7e3c53f3
DenisSever <sedihdenis@bk.ru>
2024-05-29 07:58:55 +0300

    add solution javaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1316/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task13.task1316;
+
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println(SimpleObject.NAME);
+        System.out.println(Button.NAME);
+    }
+
+    interface SimpleObject {
+        String NAME = "SimpleObject";
+    }
+
+    interface Button extends SimpleObject {
+
+        final String NAME = "Submit";
+
+        public void onPress();
+
+
+
+
+
+      String onPress(Object o);
+
+    }
+}
