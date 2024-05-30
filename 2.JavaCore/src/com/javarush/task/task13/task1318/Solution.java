6882f9f1cb6f7b26ac901fbea622d4a4dddc91ce
DenisSever <sedihdenis@bk.ru>
2024-05-30 15:15:18 +0300

    add solution javaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task13.task1318;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(string))) {
+            while (bufferedReader.ready()) {
+                String line = bufferedReader.readLine();
+                System.out.println(line);
+
+            }
+
+
+        } finally {
+            scanner.close();
+
+
+        }
+    }
+}
