6731f28ea1d31c7fe2bf168b433f11151f3f6e58
DenisSever <sedihdenis@bk.ru>
2024-06-04 23:48:36 +0300

    add solution javaSyntax task

--- 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -4,25 +4,27 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String string = scanner.nextLine();
-
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(string))) {
-            while (bufferedReader.ready()) {
-                String line = bufferedReader.readLine();
-                System.out.println(line);
+                while (bufferedReader.ready()) {
+                    String line = bufferedReader.readLine();
+                    System.out.println(line);
+                    bufferedReader.close();
+
+                }
+
+            } finally {
+                scanner.close();
 
             }
-
-
-        } finally {
-            scanner.close();
-
-
         }
     }
 }
