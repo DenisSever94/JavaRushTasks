562fb9f5004f916d0f75613d5e8dd224b2f1dc25
DenisSever <sedihdenis@bk.ru>
2024-06-16 14:54:01 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0303/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0303/Solution.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner1 = new Scanner(System.in);
-        int age = scanner1.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
         if (age < 18)
             if (age >= 6)
                 System.out.println("нужно ходить в школу");
