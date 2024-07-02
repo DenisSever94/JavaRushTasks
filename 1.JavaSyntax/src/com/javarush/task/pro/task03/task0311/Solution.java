27f245d66979a693441b6d4a5b2c44c240395bda
DenisSever <sedihdenis@bk.ru>
2024-07-02 23:50:04 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -8,14 +8,9 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double number1 = scanner.nextDouble();
-        double number2 = scanner.nextDouble();
-
-        double m = Math.abs(number1 - number2);
-
-        if (Math.abs(m) < 0.000001)
-            System.out.println("числа равны");
-        else System.out.println("числа не равны");
+        //напишите тут ваш код
+        System.out.println("числа равны");
+        //напишите тут ваш код
+        System.out.println("числа не равны");
     }
 }
