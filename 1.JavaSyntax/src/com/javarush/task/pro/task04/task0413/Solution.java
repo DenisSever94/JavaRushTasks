58c898c96416e583c2391473390e485b5426699b
DenisSever <sedihdenis@bk.ru>
2024-07-06 18:05:22 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
@@ -6,9 +6,12 @@ package com.javarush.task.pro.task04.task0413;
 
 public class Solution {
     public static void main(String[] args) {
+        // Первый цикл выстривает высоту треугольника
         for (int i = 0; i <= 9; i++) {
 
            int start = 0 + i;
+// Второй цикл прибавляет каждый новый виток 8 к предыдущему колличеситву 8
+            // выводит в консоль с новой строки
             for (int j = 0; j <= start; j++) {
 
                 System.out.print("8");
