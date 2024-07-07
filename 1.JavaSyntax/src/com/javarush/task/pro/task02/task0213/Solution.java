73185598f318950e550bf086a254ea2d8326ec3d
DenisSever <sedihdenis@bk.ru>
2024-07-07 17:16:12 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0213/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0213/Solution.java
@@ -12,9 +12,9 @@ public class Solution {
         String career = "Experienced Team Leader with strong Organizational Skills and a Successful career in Management.";
 
         //напишите тут ваш код
-        System.out.println("RESUME".toLowerCase());
-        System.out.println("TITLE: ".toLowerCase() + title.toLowerCase());
-        System.out.println("DEGREE: ".toLowerCase() + degree.toLowerCase());
-        System.out.println("CAREER: ".toLowerCase() + career.toLowerCase());
+        System.out.println("RESUME");
+        System.out.println("TITLE: " + title);
+        System.out.println("DEGREE: " + degree);
+        System.out.println("CAREER: " + career);
     }
 }
