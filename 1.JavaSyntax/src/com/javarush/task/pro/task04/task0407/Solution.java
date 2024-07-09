24fcf813d77da61ae90ae0dc5bc0818a1ae2774d
DenisSever <sedihdenis@bk.ru>
2024-07-09 16:24:28 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0407/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0407/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.task.pro.task04.task0407;
 
+/* 
+Сумма чисел, не кратных 3
+*/
+
 public class Solution {
     public static void main(String[] args) {
 
--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
@@ -1,12 +1,21 @@
 package com.javarush.task.pro.task04.task0415;
+
 import java.util.Scanner;
+
+/* 
+Площадь круга
+*/
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int radius = scanner.nextInt();
-        double pi = 3.14;
-        int result = (int) (pi * radius * radius);
+        double pi = 3.14; // так и должно умнажаться на 3.14
+        int result = (int) (pi * radius * radius); /*
+        в результате должна отбоситься дробная часть и
+        вывести челое чилсло */
+
+
         System.out.println(result);
     }
 }
