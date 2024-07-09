5bc9e9ce28757b40e591429beef195489a839444
DenisSever <sedihdenis@bk.ru>
2024-07-09 16:30:01 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -9,15 +9,7 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number1 = scanner.nextInt();
-        int number2 = scanner.nextInt();
-        int number3 = scanner.nextInt();
-
-        int result = (number1 + number2 + number3) /3;
-        System.out.println(result);
-
-
+        //напишите тут ваш код
 
     }
 }
