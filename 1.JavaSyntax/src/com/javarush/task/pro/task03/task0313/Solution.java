c9bf50e2b5250d61c71c1dbf3d2b1af2d1a45cdd
DenisSever <sedihdenis@bk.ru>
2024-07-13 19:12:32 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0313/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0313/Solution.java
@@ -1,7 +1,5 @@
 package com.javarush.task.pro.task03.task0313;
 
-import java.util.Objects;
-
 public class Solution {
     public static String string1 = "Амиго";
     public static String string2 = string1;
@@ -10,18 +8,8 @@ public class Solution {
     public static void main(String[] args) {
         String same = "ссылки на строки одинаковые";
         String different = "ссылки на строки разные";
-        if (string1 == string2) {
-            System.out.println(same);
-        } else System.out.println(different);
-
-        if (string2 == string3) {
-            System.out.println(same);
-        } else System.out.println(different);
-
-        if (string1 == string3) {
-            System.out.println(same);
-        } else System.out.println(different);
-
-
+        //напишите тут ваш код
+        System.out.println(same);
+        System.out.println(different);
     }
 }
