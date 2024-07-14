342fd53aa1fb175cda27d53d53bdf60a5310186c
DenisSever <sedihdenis@bk.ru>
2024-07-14 18:37:34 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0307/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0307/Solution.java
@@ -8,10 +8,7 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int age = scanner.nextInt();
-
-        if (age < 20 || age > 60)
-            System.out.println("можно не работать");
+        //напишите тут ваш код
+        System.out.println("можно не работать");
     }
 }
