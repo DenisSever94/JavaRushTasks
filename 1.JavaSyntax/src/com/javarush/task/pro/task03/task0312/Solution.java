4073d6d09511b8ba52c877bcad4bc6662bba9946
DenisSever <sedihdenis@bk.ru>
2024-07-17 21:49:21 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0312/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0312/Solution.java
@@ -8,14 +8,9 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.nextLine();
-        String str2 = scanner.nextLine();
-
-
-        if (str1.equals(str2)) {
-            System.out.println("строки одинаковые");
-        } else
-            System.out.println("строки разные");
+        //напишите тут ваш код
+        System.out.println("строки одинаковые");
+        //напишите тут ваш код
+        System.out.println("строки разные");
     }
 }
