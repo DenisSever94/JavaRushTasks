2ffddaf48c27fe282ea7a06276cc33f2b1952eb9
DenisSever <sedihdenis@bk.ru>
2024-07-18 19:04:56 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -8,23 +8,6 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        int number1 = scanner.nextInt();
-        int number2 = scanner.nextInt();
-        int number3 = scanner.nextInt();
-
-        if (number1 == number2 && number2 == number3) {
-            System.out.println(number1 + " " + number2 + " " + number3);
-
-        } else if (number1 == number2) {
-            System.out.println(number1 + " " + number2);
-        } else if (number2 == number3) {
-            System.out.println(number2 + " " + number3);
-        } else if (number1 == number3) {
-            System.out.println(number1 + " " + number3);
-        }
-
+        //напишите тут ваш код
     }
 }
