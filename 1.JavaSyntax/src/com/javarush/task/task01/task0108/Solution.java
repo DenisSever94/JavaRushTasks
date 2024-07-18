f31733568f144533ee56779a626af8fd24584246
DenisSever <sedihdenis@bk.ru>
2024-07-18 19:05:11 +0300

    add solution javaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/task01/task0108/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0108/Solution.java
@@ -6,18 +6,18 @@ package com.javarush.task.task01.task0108;
 
 public class Solution {
     public static void main(String[] args) {
-        int z = 3;
-        int i = 2;
+        int a = 3;
+        int b = 2;
 
        // System.out.print("два");
-        System.out.print(i);
+        System.out.print(b);
         System.out.print(" плюс ");
         //System.out.print(" минус ");
-        System.out.print(z);
+        System.out.print(a);
         //System.out.print("три");
         System.out.print(" равно ");
         //System.out.print(" будет ");
       //  System.out.print("пять");
-        System.out.print(z + i);
+        System.out.print(a + b);
     }
 }
